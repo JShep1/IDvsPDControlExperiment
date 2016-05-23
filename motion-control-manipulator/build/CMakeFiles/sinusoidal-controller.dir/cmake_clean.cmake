3558bbc9c388f93c0064be5ff165ccef115307dc
John <johnshepherd96@yahoo.com>
2016-05-23 15:10:19 -0400

    Added data to reproduce ID vs PD controller experiment

---
+++ motion-control-manipulator/build/CMakeFiles/sinusoidal-controller.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/sinusoidal-controller.dir/sinusoidal-controller.cpp.o"
+  "libsinusoidal-controller.pdb"
+  "libsinusoidal-controller.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/sinusoidal-controller.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
